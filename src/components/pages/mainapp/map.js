417aa7dd48f75b5e0f44b914bd98a0341be6e9b0
Bryan Lim <bryanjlim@outlook.com>
2018-10-21 12:47:35 -0700

    Markers (not showing)

--- src/components/pages/mainapp/map.js
+++ src/components/pages/mainapp/map.js
@@ -1,7 +1,56 @@
 import React, { Component } from 'react';
-import HEREMap from 'react-here-maps';
+import HEREMap, { Marker } from 'react-here-maps';
+import firebase from 'firebase';
  
 export class Map extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            events: [],
+        }
+
+        this.getEvents().then((events) => {
+            this.state = {
+                events: events,
+            };
+        });
+
+        this.getEvents = this.getEvents.bind(this);
+        this.eachMarker = this.eachMarker.bind(this);
+    }
+
+    getEvents() {
+        const firestore = firebase.firestore();
+        const settings = {/* your settings... */ timestampsInSnapshots: true};
+        firestore.settings(settings);
+
+        return new Promise((res, rej) => {
+            const events = [];
+            firestore.collection("events").get().then(function(querySnapshot) {
+                querySnapshot.forEach(function(doc) {
+                    const data = doc.data();
+                    data.id = doc.id;
+                    events.push(data);
+                });
+                res(events);
+            });
+        });
+    }
+
+    eachMarker(event) {
+
+        const center = { lat: event.geolocation.Latitude , lng: -event.geolocation.Longitude }
+
+        return (
+            <div>
+                <Marker {...center}>
+                    <div className="circle-marker"></div>
+                </Marker>
+            </div>
+        );
+    }
+
     render() {
         return (
             <HEREMap 
@@ -9,7 +58,9 @@ export class Map extends Component {
                 appCode="CkkqR0M-MDHxwXrOdXQ8vA"
                 center={{ lat: 47.6, lng: -122.3 }}
                 zoom={10}
-            />
+            > 
+                {this.state.events.map(this.eachMarker)}
+            </HEREMap>
         )
     }
 }
