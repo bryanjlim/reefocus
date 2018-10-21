5f18d54ed2a53c7cf8cc868256897e85e4a9f741
Preston Lee <prestonwlee@gmail.com>
2018-10-20 19:32:15 -0700

    home fixes

---
+++ src/components/pages/mainapp/leaderboard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+export class Leaderboard extends Component {
+    render() {
+        return (
+            <div>
+                <div className="container">
+                    <h1>Leaderboard</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Leaderboard;
