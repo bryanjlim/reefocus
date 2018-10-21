c917942961d57111339f405ae86c7923c8dcf990
Preston Lee <prestonwlee@gmail.com>
2018-10-20 17:36:55 -0700

    Moved images folder

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-
+import Background from '../../../../public/graphics/reefocus background 1.2.svg';
 export class Home extends Component {
     render() {
         return (
             <div>
                 <div className="container">
                     <h1 className="AppName">Reefocus</h1>
-                    
+                    <img src={Background}></img>
                 </div>
             </div>
         );
