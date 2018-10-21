38124c8b2c13af0022cbcfb4d7a6aeb84f2c0b15
Preston Lee <prestonwlee@gmail.com>
2018-10-21 12:30:59 -0700

    maps

--- src/components/pages/mainapp/map.js
+++ src/components/pages/mainapp/map.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-
+import HEREMap from 'react-here-maps';
+ 
 export class Map extends Component {
     render() {
         return (
-            <div></div>
-        );
+            <HEREMap 
+                appId="PUIfToPKq2VTuNdh4Qcp"
+                appCode="CkkqR0M-MDHxwXrOdXQ8vA"
+                center={{ lat: 47.6, lng: -122.3 }}
+                zoom={10}
+            />
+        )
     }
 }
-
 export default Map;
